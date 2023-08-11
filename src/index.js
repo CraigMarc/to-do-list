f2386edde39796b43b615ba279922cc979f8604c
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-08-11 18:56:54 -0400

    added css and linked in webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import module1 from './module1';
+import './styles.css';
+
+module1()
 console.log('hello')
---
+++ src/module1.js
@@ -0,0 +1,12 @@
+
+
+function module1() {
+    const div1 = document.createElement('div');
+    div1.classList.add('main');
+    div1.textContent = "  anonymous Pisquales customer"
+
+    return document.body.appendChild(div1);
+
+}
+
+export default module1;
