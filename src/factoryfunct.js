b7baa8e1812ce17130c5103bc8fc44baffd03d2c
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-08-16 12:59:49 -0400

    added module

---
+++ src/factoryfunct.js
@@ -0,0 +1,8 @@
+const todoFactory = (project, description, dueDate, priority) => {
+    
+    return { project, description, dueDate, priority };
+  };
+
+  export default todoFactory;
+
+ 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import module1 from './module1';
 import './styles.css';
+import todoFactory from './factoryfunct';
 
 module1()
 console.log('hello')
+
+let project1 = todoFactory('project1', 'new thing', '11/1/2023', 'high')
+console.log(project1)
