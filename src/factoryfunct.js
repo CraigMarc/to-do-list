70f2176de8c83018c72eb49dc8b78ae65c21289f
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-08-16 14:05:31 -0400

    factory function for different projects

--- src/factoryfunct.js
+++ src/factoryfunct.js
@@ -1,8 +1,45 @@
-const todoFactory = (project, description, dueDate, priority) => {
+const todoFactory = (description, dueDate, priority) => {
     
-    return { project, description, dueDate, priority };
+    return { description, dueDate, priority };
+
   };
 
+  function createProject(projectName) {
+    let newProject = projectName + 'Array'
+    
+    return window[newProject] = []
+  }
+
+  function addToProject(projectName, description, dueDate, priority) {
+   let array = projectName+'Array'
+
+
+   window[array].push(todoFactory(description, dueDate, priority))
+   return window[array]
+  }
+
   export default todoFactory;
+  createProject('project1')
+  console.log(addToProject('project1', 'project1', '11/02', 'high'))
+
+  createProject('project2')
+  console.log(addToProject('project2', 'project2', '11/02', 'high'))
+  console.log(addToProject('project2', 'project2a', '11/02', 'high'))
+  /*
+ 
+let newProject = 'project1'
+
+window[newProject] = todoFactory('hello', 'project1', 'high');
+
+console.log(project1)
+
+newProject = 'project2'
+
+window[newProject] = todoFactory('hello', 'project2', 'high');
+
+console.log(project2)*/
+
+
+
 
  
